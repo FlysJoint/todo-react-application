c10bef4024a9be4770868267937169b4f4e10427
FlysJoint <wattoo42@hotmail.com>
2020-05-09 14:03:10 +0100

    app react updated

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -15,7 +15,7 @@ function TaskItem(task) {
     <div className="card">
       <div className="card-body" id="task-body" /*style={{width: auto}}*/>
         <div className="row">
-          <h6 className="card-title col-2" id="id-text" data-toggle="tooltip" data-placement="left" title="Tick/Untick to complete/uncomplete">T</h6>
+          <h6 className="card-title col-2" id="id-text" data-toggle="tooltip" data-placement="left" title="Tick/Untick to complete/uncomplete">C</h6>
           <h6 className="card-text col-8" data-toggle="tooltip" data-placement="bottom" title="Optional deadline" display="inline">{task.deadline}</h6>
           <h6 className="card-title col-2" id="id-text" data-toggle="tooltip" data-placement="right" title="Delete task">X</h6>
           <p className="card-text col-12" id="task-text" data-toggle="tooltip" data-placement="bottom" title="Add task text">{task.text}</p>                                                 
--- src/components/TasksContainer/TasksContainer.js
+++ src/components/TasksContainer/TasksContainer.js
@@ -8,8 +8,8 @@ function TasksContainer(taskContainer) {
   return (
     <div className="container col-12 col-md-6 col-lg-4 col-xl-3 align-self-end task-box">
         <TaskItem text='task one text here' deadline='5 days' isCompleted='false'/>
-        <TaskItem text='task two text here' deadline='0 days!' isCompleted='false'/>
-        <TaskItem text='task ten text here' isCompleted='true'/>
+        <TaskItem text='task 17 text here' deadline='0 days!' isCompleted='false'/>
+        <TaskItem text='task forty text here' isCompleted='true'/>
         <h2>{taskItems} Task(s)</h2>
     </div>
   );

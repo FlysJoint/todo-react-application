c5f3b98f9c2feef3c97b4d6b4f580d53374d6b81
FlysJoint <wattoo42@hotmail.com>
2020-05-09 15:43:18 +0100

    Updates

---
+++ precache-manifest.afa6aa713bf09a1e2e685ecd7b54472f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbaf71f93898b11da65055ec042a738",
+    "url": "/todo-react-application/index.html"
+  },
+  {
+    "revision": "8101acefd4f6ca03aa56",
+    "url": "/todo-react-application/static/css/main.58985ab9.chunk.css"
+  },
+  {
+    "revision": "93c824403d1214722be6",
+    "url": "/todo-react-application/static/js/2.50be1098.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-application/static/js/2.50be1098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8101acefd4f6ca03aa56",
+    "url": "/todo-react-application/static/js/main.978283fe.chunk.js"
+  },
+  {
+    "revision": "1b2ad42fd3db0298bc10",
+    "url": "/todo-react-application/static/js/runtime-main.60bbbdb5.js"
+  }
+]);

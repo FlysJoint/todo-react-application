b5c6c068adf0ed7699e2dda2ebc32c51821f133c
FlysJoint <wattoo42@hotmail.com>
2020-05-20 00:50:43 +0100

    Updates

---
+++ precache-manifest.1f803739526d2a24bcde211b5c3608a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd53e0ebd4ce8a8dc4f9cd5404c9fa08",
+    "url": "/todo-react-application/index.html"
+  },
+  {
+    "revision": "e94301b6cd55d42ec30a",
+    "url": "/todo-react-application/static/css/main.9ea2fd24.chunk.css"
+  },
+  {
+    "revision": "36f7f622d1537fe94962",
+    "url": "/todo-react-application/static/js/2.9bd62479.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-application/static/js/2.9bd62479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94301b6cd55d42ec30a",
+    "url": "/todo-react-application/static/js/main.787302f1.chunk.js"
+  },
+  {
+    "revision": "1b2ad42fd3db0298bc10",
+    "url": "/todo-react-application/static/js/runtime-main.60bbbdb5.js"
+  },
+  {
+    "revision": "99c758aa68f55617cd52b4c9cd4159d3",
+    "url": "/todo-react-application/static/media/cross.99c758aa.svg"
+  },
+  {
+    "revision": "665926e22b76aa6339da4f7eab12c067",
+    "url": "/todo-react-application/static/media/delete.665926e2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo-react-application/precache-manifest.2965432457b833f564f0ef7f5c3cac94.js"
+  "/todo-react-application/precache-manifest.1f803739526d2a24bcde211b5c3608a4.js"
 );
 
 self.addEventListener('message', (event) => {

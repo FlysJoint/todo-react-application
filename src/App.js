cc37d023f0e010b00fe943748c1182c1589f97e6
FlysJoint <wattoo42@hotmail.com>
2020-06-21 20:37:06 +0100

    PUT working save refresh bug

--- src/App.js
+++ src/App.js
@@ -9,23 +9,25 @@ function App() {
 
   // delete function needs confirmation step
   // regex task str length
-// seconds since 1970 so subtraction should work
+  // seconds since 1970 so subtraction should work
   // need an add shelf button
   // each shelf needs a default addtask bottle in addition to any bottles
   // bottle needs to remain open even if only contains new tasks
+  // d functionality
 
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    axios
-      .get("https://pgbx7na299.execute-api.eu-west-2.amazonaws.com/dev/tasks")
-      .then(res => {
-        setTasks(res.data.tasks);
-      })
-      .catch(err => {
-        console.log("Could not fetch tasks", err);
-      });
+      axios
+        .get("https://pgbx7na299.execute-api.eu-west-2.amazonaws.com/dev/tasks")
+        .then(res => {
+          setTasks(res.data.tasks);
+        })
+        .catch(err => {
+          console.log("Could not fetch tasks", err);
+        });
   }, []);
+
 
   function addTask(text, dueDate, shelf, bottle, pos) {
     const newTask = { 
@@ -38,24 +40,10 @@ function App() {
       "username": "bob5" 
     }
 
-    // const newTask = { 
-    // "text": "still get front end post to work",
-    // "dueDate": "",
-    // "isCompleted": false,
-    // "shelf": "morning",
-    // "bottle": 6,
-    // "pos": 0,
-    // "username": "bob5"
-    // }
-
-
-  //console.log(newTask);
-
     axios
     .post('https://pgbx7na299.execute-api.eu-west-2.amazonaws.com/dev/tasks', newTask)
     .then(// If the request is successful, get the task id and add it to the new task object
       (res) => {
-        //newTask.taskID = res.data.tasks.taskID;
         const updatedTasks = [ ...tasks, newTask ];
         setTasks( updatedTasks );
       }
@@ -63,15 +51,7 @@ function App() {
     .catch((err) => {
       console.log('Error in adding a task', err)
     })
-    .finally(
-      //console.log(newTask)
-    )
-
-    // const updatedTasks = [ ...tasks, newTask ]
-    // setTasks(updatedTasks);
   }
-
-// ---------------------------
 
 function completeTask(id) {
 
@@ -83,58 +63,37 @@ function completeTask(id) {
       return task;
     });
 
-  // const staticTasks = tasks.filter(task => task.taskID !== id);
-  //const updatedTasks = tasks.filter(task => task.taskID === id); // it's an array not the object!!! need to change iscomplete
-  // console.log(updatedTasks);
-
-
-  //   const updatedTasks = { 
-  //   "text": "is it username again",
-  //   "shelf": "morning",
-  //   "bottle": 6,
-  //   "pos": 0,
-  //   "isCompleted": false,
-  //   "username": "bob5"
-  // }
-
-    //console.log (updatedTasks);
+    const updatedTask = tasks.filter(task => task.taskID === id);
+    console.log(updatedTask);
 
     axios
     .put(
-      `https://pgbx7na299.execute-api.eu-west-2.amazonaws.com/dev/tasks/${id}`, updatedTasks[0]
+      `https://pgbx7na299.execute-api.eu-west-2.amazonaws.com/dev/tasks/${id}`, updatedTask[0]
     )
     .then(res => {
       // There is probably no data returned from a Put request.
-      //console.log(res.data);
       // But if you're in the "then" function you know the request succeeded.
       console.log('complete ' + id + ' clicked');
+
+      // axios
+      //   .get("https://pgbx7na299.execute-api.eu-west-2.amazonaws.com/dev/tasks")
+      //   .then(res => {
+      //     setTasks(res.data.tasks);
+      //   })
+      //   .catch(err => {
+      //     console.log("Could not fetch tasks", err);
+      //   });
+
     })
     .catch(err => {
       console.log("Error updating task " + id, err);
     });
 
-    // console.log(tasks);
-    // console.log(updatedTasks);
     setTasks( updatedTasks );
 
   }
-  
-// -----------------------------------
-
-  // function completeTask(id) {
-  //   console.log('complete ' + id + ' clicked');
-  //   const updatedTasks = tasks.map(task => {
-  //     if(task.taskID === id) {
-  //       !task.isCompleted ? task.isCompleted = true : task.isCompleted = false;
-  //     }
-  //     return task;
-  //   });
-  //   setTasks(updatedTasks);
-  // }
 
   function deleteTask(id) {
-    // const updatedTasks = tasks.filter(task => task.taskID !== id);
-    // setTasks(updatedTasks);
 
     axios
       .delete(`https://pgbx7na299.execute-api.eu-west-2.amazonaws.com/dev/tasks/${id}`)
@@ -147,7 +106,6 @@ function completeTask(id) {
       .catch(err => {
         console.log("Could not delete task", err);
       });
-
   }
 
   function getShelfTasks(tasks) {
